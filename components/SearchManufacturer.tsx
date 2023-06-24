19895109ac470884f36f964457c95694ffc32c4d
julien-muke <julien.muke@gmail.com>
2023-06-24 13:49:24 +0200

    car catalogue

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
 
   return (
     <div className='search-manufacturer'>
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className='relative w-full'>
           <Combobox.Button className="absolute top-[14px]">
             <Image 
@@ -49,16 +49,7 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
           afterLeave={() => setQuery('')}
           >
             <Combobox.Options>
-              {filteredManufacturers.length === 0 &&
-              query !== "" ? (
-                <Combobox.Option
-                value={query}
-                className="search-manufacturer__option"
-                >
-                  Create "{query}"
-                </Combobox.Option>
-              ): (
-                filteredManufacturers.map((item) => (
+              {filteredManufacturers.map((item) => (
                   <Combobox.Option
                     key={item}
                     className={({ active }) => `
@@ -67,10 +58,28 @@ const SearchManufacturer = ({ manufacturer, setManufacturer }: SearchManufacture
                     `}
                     value={item}
                   >
-                    {item}
-                  </Combobox.Option>
+                    {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+                  </Combobox.Option> 
                 )
-              ))}
+              )}
             </Combobox.Options>
           </Transition>
         </div>
